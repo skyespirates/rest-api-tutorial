1b008e000dd328d0203227ece15ec4ad66c8d06a
Skyes Crawford <akashiseijurou414@gmail.com>
2020-11-04 10:58:08 +0700

    done

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MONGODB_URI:
-    "mongodb://skyes:W2Cq4dke0zk0ceMX@cluster0-shard-00-00.xmbsx.mongodb.net:27017,cluster0-shard-00-01.xmbsx.mongodb.net:27017,cluster0-shard-00-02.xmbsx.mongodb.net:27017/restapi?ssl=true&replicaSet=atlas-5k5v1v-shard-0&authSource=admin&retryWrites=true&w=majority",
-};
