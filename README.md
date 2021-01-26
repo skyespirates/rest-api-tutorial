40a69a65c6c36e051543d6eb73b09dd0a51e686f
Skyes Crawford <akashiseijurou414@gmail.com>
2021-01-26 22:47:33 +0700

    add images

---
+++ README.md
@@ -0,0 +1,2 @@
+![posts](https://github.com/skyespirates/rest-api-tutorial/blob/master/images/posts.JPG)
+![todos](https://github.com/skyespirates/rest-api-tutorial/blob/master/images/todos.JPG)
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./config.js");
 const app = express();
+const cors = require("cors");
 
 //  BodyParser Middleware
+app.use(cors());
 app.use(express.json());
 const postsRoutes = require("./routes/api/posts");
 const todosRoutes = require("./routes/api/todos");
@@ -26,5 +28,5 @@ app.get("/", (req, res) => {
 app.use("/api/posts", postsRoutes);
 app.use("/api/todos", todosRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
